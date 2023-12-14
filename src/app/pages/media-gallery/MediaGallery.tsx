440ea796ec5081919c9557d5bedc304424a493cf
rohitborkarbsl <rohit.borkar@bluespacelabs.com>
2023-12-14 10:59:24 +0530

    MOdules-Dashboard,Media&Gallery files

---
+++ src/app/pages/media-gallery/MediaGallery.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { PageTitle } from "../../../_metronic/layout/core";
+
+const MediaGallery: FC = () => {
+  return (
+    <>
+      <PageTitle breadcrumbs={[]}>Media Gallery</PageTitle>
+    </>
+  );
+};
+
+export default MediaGallery;

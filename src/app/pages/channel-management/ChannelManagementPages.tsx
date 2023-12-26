1031231a5a251a196673c36ed373d4d833f514bd
Subhro Mukherjee <86202754+MsubhroSDE@users.noreply.github.com>
2023-12-26 20:22:37 +0530

    chores : channels - dialog flow cx tab added.

--- src/app/pages/channel-management/ChannelManagementPages.tsx
+++ src/app/pages/channel-management/ChannelManagementPages.tsx
@@ -3,6 +3,7 @@ import WhatsappPage from "./WhatsappPage/WhatsappPage";
 import TelegramPage from "./TelegramPage/TelegramPage";
 import InstagramPage from "./InstagramPage/InstagramPage";
 import FacebookPage from "./FacebookPage/FacebookPage";
+import DialogFlowCXPage from "./DialogFlowCX/DialogFlowCXPage";
 
 const ChannelManagementPages = ({ channelTab }: any) => {
   if (channelTab === "whatsapp") {
@@ -13,6 +14,8 @@ const ChannelManagementPages = ({ channelTab }: any) => {
     return <FacebookPage />;
   } else if (channelTab === "instagram") {
     return <InstagramPage />;
+  } else if (channelTab === "dialogFlowCX") {
+    return <DialogFlowCXPage />;
   } else {
     return null;
   }
